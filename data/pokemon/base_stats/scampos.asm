396a87e617af5ff11ec0907a6a10d4f604fd8b2a
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-03-16 08:19:21 -0400

    Update scampos.asm

--- data/pokemon/base_stats/scampos.asm
+++ data/pokemon/base_stats/scampos.asm
@@ -1,6 +1,6 @@
 +	db DEX_SCAMPOS ; pokedex id
 +
-+	db  85,  90,  80,  130,  80
++	db  40,  55,  35,  60,  35
 +	;   hp  atk  def  spd  spc
 +
 +	db NORMAL, NORMAL ; type
@@ -10,11 +10,11 @@
 +	INCBIN "gfx/pokemon/front/scampos.pic", 0, 1 ; sprite dimensions
 +	dw ScamposPicFront, ScamposPicBack
 +
-+	db LEECH_LIFE, SCREECH, BITE, SCREECH ; level 1 learnset
++	db SCRATCH, TAIL_WHIP, NO_MOVE, NO_MOVE ; level 1 learnset
 +	db GROWTH_MEDIUM_FAST ; growth rate
 +
 +	; tm/hm learnset
-+	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
++	tmhm CUT,   BODY_SLAM,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
 +	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   MIMIC,        DOUBLE_TEAM,  \
 +	     BIDE,         SWIFT,        REST,         SUBSTITUTE,   FLY
 +	; end
